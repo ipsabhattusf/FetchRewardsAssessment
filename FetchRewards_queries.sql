c85448bd6781a17f7a2527caad0308e99254f3c6
ipsabhattusf <85321995+ipsabhattusf@users.noreply.github.com>
2022-02-09 15:25:34 -0500

    Add files via upload

--- FetchRewards_queries.sql
+++ FetchRewards_queries.sql
@@ -8,7 +8,7 @@ from receipts r,
 where createddate>='01-SEP-20') u where u.id=r.userid)) r 
 where b.brandcode=r.itl_brandcode;
 
-/*Which brand has the most spend among users who were created within the past 6 months?*/
+/*Which brand has the most transactions among users who were created within the past 6 months?*/
 with innrq as (select count(*) cnt,itl_brandcode from rrecepts r, (select * from users
 where createddate>='01-SEP-20') u where u.id=r.userid
 group by itl_brandcode order by cnt desc)
